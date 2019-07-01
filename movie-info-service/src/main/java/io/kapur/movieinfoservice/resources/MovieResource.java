efc961c3df395ed06ff18509a225fdaad880d010
Kapur <pkapur@informatica.com>
2019-07-02 01:36:50 +0530

    Added Movie Info Service API

---
+++ movie-info-service/src/main/java/io/kapur/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,20 @@
+package io.kapur.movieinfoservice.resources;
+
+import io.kapur.movieinfoservice.models.Movie;
+
+import java.util.Collections;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+	@RequestMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+		return new Movie(movieId, "Titanic", "Romantic Movie");
+	}
+
+}
