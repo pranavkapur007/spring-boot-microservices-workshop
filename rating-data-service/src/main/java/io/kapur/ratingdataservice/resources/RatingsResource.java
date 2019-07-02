02519bb5c84939a4a8ecebb2b5e92aeddfd4c44c
Kapur <pkapur@informatica.com>
2019-07-02 12:07:04 +0530

    Added Rating Data Service API

---
+++ rating-data-service/src/main/java/io/kapur/ratingdataservice/resources/RatingsResource.java
@@ -0,0 +1,17 @@
+package io.kapur.ratingdataservice.resources;
+
+import io.kapur.ratingdataservice.models.Rating;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+	
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId) {
+		return new Rating(movieId, 4);
+	}
+}
