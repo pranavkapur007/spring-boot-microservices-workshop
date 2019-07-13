28aa6756eb2727dd5042dd642a3df48199a72a43
Kapur <pkapur@informatica.com>
2019-07-13 17:42:40 +0530

    Using Rest Template to call Movie Info API

--- movie-catalog-service/src/main/java/io/kapur/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/io/kapur/moviecatalogservice/resources/CatalogResource.java
@@ -6,11 +6,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import io.kapur.moviecatalogservice.models.CatalogItem;
+import io.kapur.moviecatalogservice.models.Movie;
 import io.kapur.moviecatalogservice.models.Rating;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/catalog")
@@ -18,22 +20,31 @@ public class CatalogResource {
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+		RestTemplate restTemplate = new RestTemplate();
+
 		List<Rating> ratingList = new ArrayList<Rating>();
 		ratingList.add(new Rating("1234", 3));
 		ratingList.add(new Rating("5678", 4));
 
 		return ratingList
 				.stream()
-				.map(rating -> new CatalogItem("Name", "Desc", rating
-						.getRating())).collect(Collectors.toList());
-		
-		
-		 /* Alternative way, valid for jdk 1.7
-		  * List<CatalogItem> catelogItem =new ArrayList<CatalogItem>();
-		 for(Rating rate: ratingList){
-			 catelogItem.add("Name", "Desc", rate.getRating());
-		 }
-		 return catelogItem; */
-		 
+				.map(rating -> {
+					Movie movie = restTemplate.getForObject(
+							"http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+					return new CatalogItem(movie.getName(), movie
+							.getDescription(), rating.getRating());
+				}).collect(Collectors.toList());
+
+		/*
+		 * Alternative way, valid for jdk 1.7 
+		 * List<CatalogItem> catelogItem =new ArrayList<CatalogItem>();
+		 * Movie movie; 
+		 * for(Rating rate: ratingList){
+		 * 	movie=restTemplate.getForObject("https://localhost:8081/movies/", Movie.class);
+		 * catelogItem.add(movie.getName(), movie.getDescription(), rate.getRating()); } 
+		 * return catelogItem;
+		 */
+
 	}
 }
